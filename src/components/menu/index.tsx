584c65f56838026777775c5b125de7bb1531c454
Ademir Stefanski <ademirstefanski@hotmail.com>
2024-03-07 12:21:48 -0300

    transformation of the menu list into responsive.

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,31 +1,63 @@
 import { useContext } from "react";
-import { 
-  Container,
-  MenuItem
-  
-} from "./styles";
+import { Container, MenuItem, ButtonIcon } from "./styles";
 import { MenuContext } from "../../contexts/menuContext";
 
-const items =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] //array simulando a lista de itens do menu que viria do backend;
+import Home from "../../assets/home_icon.png";
+import ShortsIcon from "../../assets/shorts_icon.png";
+import SubscriptionsIcon from "../../assets/sino.png";
+import YoutubeMusicIcon from "../../assets/sino.png";
+import YourChannelIcon from "../../assets/sino.png";
+import HistoryIcon from "../../assets/sino.png";
+import YourVideosIcon from "../../assets/sino.png";
+import WatchLaterIcon from "../../assets/sino.png";
+import DownloadsIcon from "../../assets/sino.png";
+import TrendingIcon from "../../assets/sino.png";
+import MusicIcon from "../../assets/sino.png";
+import MoviesIcon from "../../assets/sino.png";
+import LiveIcon from "../../assets/sino.png";
+import GamingIcon from "../../assets/sino.png";
+import NewsIcon from "../../assets/sino.png";
+import SportsIcon from "../../assets/sino.png";
+import LearningIcon from "../../assets/sino.png";
+import PodcastsIcon from "../../assets/sino.png";
+import SettingsIcon from "../../assets/sino.png";
+import ReportHistoryIcon from "../../assets/sino.png";
+import HelpIcon from "../../assets/sino.png";
+import SendFeedbackIcon from "../../assets/sino.png";
+
+//const items =[0,0,0] //array simulando a lista de itens do menu que viria do backend;
+
+const items = [
+  "Home",
+  "Shorts",
+  "Subscriptions",
+  "Your channel",
+  "History",
+  "Your Videos",
+  "Watch Later",
+];
+
+const itemOBJ = {
+  Home: "HomeIcon",
+  Shorts: "ShortsIcon",
+  Subscriptions: "SubscriptionsIcon",
+};
 
 
 
 function Menu() {
-  const {menuState} = useContext(MenuContext);    
-  
+  const { menuState } = useContext(MenuContext);
+
   return (
     <Container menuState={menuState}>
-      {items.map(() => (
-        <MenuItem>
-          Início
+      {items.map((item) => (
+        <MenuItem menuState={menuState}>
+          <ButtonIcon alt="" src={Home} />
+          <span>{item}</span>
         </MenuItem>
       ))}
-
-
     </Container>
-  )
+  );
 }
 
-
 export default Menu;
-
--- src/components/menu/styles.ts
+++ src/components/menu/styles.ts
@@ -11,14 +11,30 @@ flex-direction: column;
 overflow-y: auto; //quando os itens ultrapassarem o limite da div, será criado um scroll automaticamente.  
 `;
 
-export const MenuItem = styled.div`
+export const MenuItem = styled.div<{menuState: boolean}>`
 width: 90%;
-min-height: 40px;
+min-height: ${({ menuState }) =>menuState? "40px" : "70px"};
 border-radius: 10px;
 cursor: pointer;
+padding: 2px 17px;
+box-sizing: border-box;
+display: flex;
+flex-direction: ${({ menuState }) =>menuState? "row" : "column"};
+align-items: center;
+justify-content: ${({ menuState }) =>menuState? "none" : "center"};
+
+span {
+  font-weight: ${({ menuState }) =>menuState? "600" : "400"};
+  margin-left: ${({ menuState }) =>menuState? "20px" : "none"};
+  font-size: ${({ menuState }) =>menuState? "16px" : "12px"};
+  text-align: ${({ menuState }) =>menuState? "none" : "center"};
+}
 
   &:hover {
       background-color: #f2f2f2;
   }
 `;
 
+export const ButtonIcon = styled.img`
+  width: 25px;
+`;
